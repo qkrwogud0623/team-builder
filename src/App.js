17acd119d5c871beeea283466d9ecbf7803267c1
Jaehyeong Park <qkrwogud0623@ajou.ac.kr>
2025-08-27 00:24:22 +0900

    Remove API keys from source code

--- src/App.js
+++ src/App.js
@@ -23,12 +23,12 @@ import {
 
 // --- Firebase Configuration ---
 const firebaseConfig = {
-  apiKey: "AIzaSyB20tMnlaAaHQ6qEGloTDs7NJPBqnDNGWg",
-  authDomain: "musicbuilder-a04bb.firebaseapp.com",
-  projectId: "musicbuilder-a04bb",
-  storageBucket: "musicbuilder-a04bb.appspot.com",
-  messagingSenderId: "208067005546",
-  appId: "1:208067005546:web:f4c26a1471b24488594e13"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 // Initialize Firebase
