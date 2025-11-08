ce2c45c8f4f8139a7810d898bac3b85dee7d9da1
Jaehyeong Park <qkrwogud0623@ajou.ac.kr>
2025-11-08 22:24:23 +0900

    fix: (진짜최종) GHPages base path 수정

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/team-builder/', // [!!!] 이게 제일 중요합니다 [!!!]
+  base: '/team-builder/', // [!!!] GHPages용 하위 폴더 이름으로 수정 [!!!]
 })
