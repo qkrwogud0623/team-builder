503fa4e7c95d07ea9329499260e85f86bdd75345
Jaehyeong Park <qkrwogud0623@ajou.ac.kr>
2025-11-08 22:35:11 +0900

    refactor: (진짜최종) GHPages 로컬 배포로 완전 전환

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/team-builder/', // [!!!] GHPages용 하위 폴더 이름으로 수정 [!!!]
+  base: '/team-builder/', // [!!!] 이게 제일 중요합니다 [!!!]
 })
